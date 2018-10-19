f0cd563c8ce5ae11610822ee68470e07e0098d72
Jarosław Gwizdała <Jaroslaw.Gwizdala@powel.no>
2018-10-19 15:06:10 +0200

    creating observable with custom unsubscribe and share

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ObservablesService } from './services/observables.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'observable-poc';
+  title = 'Observable POC';
+
+  constructor(private service: ObservablesService) {}
+  
+  async demo1() {
+    console.log('Firing demo without custom unsubscribe');
+    let sub = this.service.getWithoutCustomUnsubscribe().subscribe(x => console.log('oudside 1: ',x));
+    await wait(5000);
+    let sub2 = this.service.getWithoutCustomUnsubscribe().subscribe(x => console.log('outside 2: ',x));
+    await wait(5000);
+    sub.unsubscribe();
+    await wait(2100);
+    sub2.unsubscribe();
+  }
+
+  async demo2() {
+    console.log('Firing demo with custom unsubscribe');
+    let sub = this.service.getWithCustomUnsubscribe().subscribe(x => console.log('oudside 1: ',x));
+    await wait(5000);
+    let sub2 = this.service.getWithCustomUnsubscribe().subscribe(x => console.log('outside 2: ',x));
+    await wait(5000);
+    sub.unsubscribe();
+    await wait(2100);
+    sub2.unsubscribe();
+  }
+
+  async demo3() {
+    console.log('Firing demo with share');
+    let sub = this.service.getWithShare().subscribe(x => console.log('oudside 1: ',x));
+    await wait(5000);
+    let sub2 = this.service.getWithShare().subscribe(x => console.log('outside 2: ',x));
+    await wait(5000);
+    sub.unsubscribe();
+    await wait(2100);
+    sub2.unsubscribe();
+    await wait(1000);
+    let sub3 = this.service.getWithShare().subscribe(x => console.log('oudside 3: ',x));
+    await wait(4000);
+    sub3.unsubscribe();
+  }
+}
+
+function wait(wait: number): Promise<void> {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => resolve(),wait);
+  })
 }
---
+++ src/app/services/observables.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@angular/core";
+import { Observable, Observer } from "rxjs";
+import { share } from "rxjs/operators";
+
+
+@Injectable({
+    providedIn: "root"
+})
+export class ObservablesService {
+
+    private observable1$: Observable<number>;
+    private observable2$: Observable<number>;
+    private observable3$: Observable<number>;
+
+
+    constructor() {
+        this.observable1$ = Observable.create((observer: Observer<any>) => {
+            let counter = 0;
+            setInterval(() => {
+                counter += 1;
+                console.log('internal without custome unsubscribe',counter);
+                observer.next(counter);
+                if (counter === 11)
+                    console.warn('You need to reload the page to get rid of it');
+            },1000);
+        });
+
+        this.observable2$ = Observable.create((observer: Observer<any>) => {
+            let counter = 0;
+            let interval = setInterval(() => {
+                counter += 1;
+                console.log('internal with custom unsubscribe',counter);
+                observer.next(counter);
+            },1000);
+            return () => clearInterval(interval);
+        })
+
+        this.observable3$ = Observable.create((observer: Observer<any>) => {
+            let counter = 0;
+            let interval = setInterval(() => {
+                counter += 1;
+                console.log('internal with share',counter);
+                observer.next(counter);
+            },1000);
+            return () => clearInterval(interval);
+        }).pipe(share())
+    }
+
+    getWithoutCustomUnsubscribe() {
+        return this.observable1$;
+    }
+
+    getWithCustomUnsubscribe() {
+        return this.observable2$;
+    }
+
+    getWithShare() {
+        return this.observable3$;
+    }
+}
